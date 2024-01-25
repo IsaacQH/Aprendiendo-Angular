6ecf18bfe6629b09a619e088ae29dd367b147598
IsaacQH <isaacqwert@gmail.com>
2024-01-25 02:22:02 -0600

    Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+//Importar modulos de router de angular
 
-export const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+
+//Importar conmponentes (carpetas.componente)
+
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { homeComponent } from './home/home.component';
+
+//Array de rutas
+export const routes: Routes = [
+    {path: '', component: homeComponent},                       //Este carga la ruta vacia, la home
+    {path: 'zapatillas', component: ZapatillasComponent},       //Este carga componente zapatilla
+    {path: 'videojuego', component: VideojuegoComponent},       //Este carga componente videojuego
+    {path: 'curso', component: CursosComponent},                //Este carga componente curso
+    {path: '**' , component: homeComponent}                        //Ruta en caso de error
+];
